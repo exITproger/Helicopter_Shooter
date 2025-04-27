4a38b287596231a999ea7b2ada9ba997750943ec
exITproger <fuvfuv2024@gmail.com>
2025-04-27 12:52:07 +0300

    #1 сделал

---
+++ HelicopterShooter/GameForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelicopterShooter
+{
+    public partial class GameForm : Form
+    {
+        public GameForm()
+        {
+            InitializeComponent();
+            KeyPreview = true;
+            KeyDown += GameForm_KeyDown;
+        }
+
+        private void GameForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                MainMenuForm mainMenu = new MainMenuForm();
+                mainMenu.Show();
+                Close();
+            }
+        }
+
+    }
+}
